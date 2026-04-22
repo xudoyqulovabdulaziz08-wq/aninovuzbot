f1d98774f69a4419fb7201ff6f45964f88fbe2f4
Abdulaziz Xudoyqulov <Xudoyqulovabdulaziz@example.com>
2026-04-22 17:56:02 +0500

    Save local changes before rebase

--- database/connection.py
+++ database/connection.py
@@ -1,25 +1,18 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
-from sqlalchemy.orm import DeclarativeBase
+from config import config
 
-# PostgreSQL URL manzilingiz (asyncpg drayveri bilan)
-# DIQQAT: 'postgresql://' ni 'postgresql+asyncpg://' ga o'zgartirdik
-DATABASE_URL = "postgresql+asyncpg://avnadmin:AVNS_NL2rxYz4-DWFJ0f4Fme@pg-32706ea0-xudoyqulovabdulaziz08-0be3.h.aivencloud.com:27624/defaultdb?sslmode=require"
-
-# 1. Engine yaratish (Bazaga ulanish yo'lagi)
+# ✅ URL .env faylidan olinadi
 engine = create_async_engine(
-    DATABASE_URL,
-    echo=False, # Loglarda har bir SQL so'rovni ko'rmaslik uchun False
-    future=True
+    config.DATABASE_URL,
+    connect_args={"ssl": True}
 )
 
-# 2. Sessionmaker yaratish (Har bir xabar uchun alohida sessiya ochish uchun)
 AsyncSessionLocal = async_sessionmaker(
     bind=engine,
     class_=AsyncSession,
     expire_on_commit=False
 )
 
-# 3. Bazaga ulanishni tekshirish funksiyasi (ixtiyoriy)
 async def check_db():
     try:
         async with engine.connect() as conn:
--- middlewares/db_middleware.py
+++ middlewares/db_middleware.py
@@ -50,4 +50,3 @@ class DbSessionMiddleware(BaseMiddleware):
 
             return await handler(event, data)
         
-
